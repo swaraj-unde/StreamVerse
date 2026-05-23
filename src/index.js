f253828d60ce9ec26e12e6f071e0fddb20a57c76
Swaraj <swarajunde1@gmail.com>
2026-05-23 19:54:48 +0530

    CORS and All

--- src/index.js
+++ src/index.js
@@ -1,11 +1,21 @@
 import dotenv from 'dotenv';
 import connectDB from './db/index.js';
+import app from './app.js';
 
 dotenv.config({
     path: './env',
 });
 
-connectDB();
+connectDB()
+    .then(() => {
+        const PORT = process.env.PORT || 8000;
+        app.listen(PORT, () => {
+            console.log(`Server is listening on port ${PORT}`);
+        });
+    })
+    .catch((err) => {
+        console.error('Error connecting to MongoDB:', err);
+    });
 
 // import express from 'express';
 
